e029147bef14a8988b4aa625b30022833ac1a918
Aprikot <mandani850@gmail.com>
2023-02-07 11:19:41 +0700

    - add app, flutter, flutter plugin registrant xcframework

--- Package.swift
+++ Package.swift
@@ -25,6 +25,21 @@ let package = Package(
           name: "SociomileSDK",
           path: "./Sources/SociomileSDK.xcframework"
         ),
+        .binaryTarget(
+          name: "App",
+          url: "https://sociomile-sdk.s3.ap-southeast-1.amazonaws.com/sociomile-ios-sdk/App.xcframework.zip",
+          checksum: "786c03b25195ab44823691822a84218a517e5afe32251353d70da1975733a9e3"
+        ),
+        .binaryTarget(
+          name: "Flutter",
+          url: "https://sociomile-sdk.s3.ap-southeast-1.amazonaws.com/sociomile-ios-sdk/Flutter.xcframework.zip",
+          checksum: "2533eb6f14c3f979d45ece6c356e06c91d771a804b0039083caebd2e8691a563"
+        ),
+        .binaryTarget(
+          name: "FlutterPluginRegistrant",
+          url: "https://sociomile-sdk.s3.ap-southeast-1.amazonaws.com/sociomile-ios-sdk/FlutterPluginRegistrant.xcframework.zip",
+          checksum: "0688f45240b2034e137464e39a70e38de58d5fe759cb85c1ad4a4949f0e8d489"
+        ),
 //        .target(
 //            name: "SociomileSDK",
 //            dependencies: []),
