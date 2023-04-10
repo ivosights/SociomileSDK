f78aeb818006d56b1946a82605d8b48c1b1ac82a
Aprikot <mandani850@gmail.com>
2023-04-10 08:40:14 +0700

    remove app, flutter, & flutter plugin registrant binary target

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SociomileSDK",
-            targets: ["SociomileSDKTarget"]),//, "App", "Flutter", "FlutterPluginRegistrant"]),
+            targets: ["SociomileSDK"]),//, "App", "Flutter", "FlutterPluginRegistrant"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,12 +21,12 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .binaryTarget(
-//          name: "SociomileSDK",
-//          url: "https://sociomile-sdk.s3.ap-southeast-1.amazonaws.com/sociomile-ios-sdk/sandbox/0.0.2/SociomileSDK.xcframework.zip",
-//          checksum: "89d7aa26e90ea100f931e21c27f3425351bc662e9b42bd1e465b66b98a5feffa"
-////          path: "./Sources/SociomileSDK.xcframework"
-//        ),
+        .binaryTarget(
+          name: "SociomileSDK",
+          url: "https://sociomile-sdk.s3.ap-southeast-1.amazonaws.com/sociomile-ios-sdk/sandbox/0.0.2/SociomileSDK.xcframework.zip",
+          checksum: "89d7aa26e90ea100f931e21c27f3425351bc662e9b42bd1e465b66b98a5feffa"
+//          path: "./Sources/SociomileSDK.xcframework"
+        ),
 //        .binaryTarget(
 //          name: "App",
 //          url: "https://sociomile-sdk.s3.ap-southeast-1.amazonaws.com/sociomile-ios-sdk/sandbox/0.0.2/App.xcframework.zip",
@@ -42,16 +42,16 @@ let package = Package(
 //          url: "https://sociomile-sdk.s3.ap-southeast-1.amazonaws.com/sociomile-ios-sdk/sandbox/0.0.2/FlutterPluginRegistrant.xcframework.zip",
 //          checksum: "44668d9c0f3f34dc0de0f73ff291aceb1355bd5fa1b1948cd3afa68cd3e13464"
 //        ),
-        .target(
-            name: "SociomileSDKTarget",
-//            dependencies: [
-//                .target(name: "SociomileSDK"),
-//                .target(name: "App"),
-//                .target(name: "Flutter"),
-//                .target(name: "FlutterPluginRegistrant")
-//            ],
-            path: "Sources"
-        )
+//        .target(
+//            name: "SociomileSDKTarget",
+////            dependencies: [
+////                .target(name: "SociomileSDK"),
+////                .target(name: "App"),
+////                .target(name: "Flutter"),
+////                .target(name: "FlutterPluginRegistrant")
+////            ],
+//            path: "Sources"
+//        )
 //        .testTarget(
 //            name: "SociomileSDKTests",
 //            dependencies: ["SociomileSDK"]),
